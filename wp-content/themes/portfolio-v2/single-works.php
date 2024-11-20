d4f4891ab9c37661fa601a4b6b3712d8736ecb6b
marumaru <yukinaka3009@gmail.com>
2024-11-20 21:04:27 +0900

    2024/11/20 詳細ページ完成

--- wp-content/themes/portfolio-v2/single-works.php
+++ wp-content/themes/portfolio-v2/single-works.php
@@ -1,196 +1,212 @@
 <?php get_header(); ?>
 <main id="single-works"
-      class="pages-main single-main">
-  <div class="single-top">
-    <div class="section_bg">
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-    <div class="window-click-contents window-modal-open">
-      <img src="<?php echo get_theme_file_uri('/images/top/web-app/app-sample.webp'); ?>"
-           alt="" />
-    </div>
-    <div class="window-modal">
-      <div class="modal-close window-modal-close">×</div>
-      <div class="window-contents window-contents-works pc-view">
-        <ul class="tab-menu">
-          <li class="tab-menu_item tab-menu_switch">
-            <span class="material-symbols-outlined"> devices </span>
-          </li>
-          <li class="tab-menu_item tab is-active">
-            <div class="item_inner">
-              <span class="material-symbols-outlined">globe</span>
-              <span class="tab-text">Webサイト制作例1</span>
-              <div class="tab-menu_line"></div>
-            </div>
-          </li>
-          <li class="tab-menu_item tab">
-            <div class="item_inner">
-              <span class="material-symbols-outlined">globe</span>
-              <span class="tab-text">Webサイト制作例2</span>
-              <div class="tab-menu_line"></div>
-            </div>
-          </li>
-          <li class="tab-menu_item tab">
-            <div class="item_inner">
-              <span class="material-symbols-outlined">globe</span>
-              <span class="tab-text">Webサイト制作例3</span>
-              <div class="tab-menu_line"></div>
-            </div>
-          </li>
-          <li class="tab-menu_item tab">
-            <div class="item_inner">
-              <span class="material-symbols-outlined">globe</span>
-              <span class="tab-text">Webサイト制作例4</span>
-              <div class="tab-menu_line"></div>
-            </div>
-          </li>
-        </ul>
-        <div class="tab-content is-display">
-          <img class="pc-view"
-               src="<?php echo get_theme_file_uri('/images/top/tab/pc/twenty5-pc.webp'); ?>"
-               alt=""
-               loading="lazy" />
-          <img class="sp-view"
-               src="<?php echo get_theme_file_uri('/images/top/tab/sp/twenty5-sp.webp'); ?>"
-               alt=""
-               loading="lazy" />
+  class="pages-main single-main">
+  <?php if (have_posts()): while (have_posts()): the_post(); ?>
+      <div class="single-top">
+        <div class="section_bg">
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
-        <div class="tab-content">
-          <img class="pc-view"
-               src="<?php echo get_theme_file_uri('/images/top/tab/pc/designpiano-pc.webp'); ?>"
-               alt=""
-               loading="lazy" />
-          <img class="sp-view"
-               src="<?php echo get_theme_file_uri('/images/top/tab/sp/designpiano-sp.webp'); ?>"
-               alt=""
-               loading="lazy" />
+        <div class="window-click-contents window-modal-open">
+          <?php the_post_thumbnail('full') ?>
         </div>
-        <div class="tab-content">
-          <img class="pc-view"
-               src="<?php echo get_theme_file_uri('/images/top/tab/pc/twenty5-pc.webp'); ?>"
-               alt=""
-               loading="lazy" />
-          <img class="sp-view"
-               src="<?php echo get_theme_file_uri('/images/top/tab/sp/twenty5-sp.webp'); ?>"
-               alt=""
-               loading="lazy" />
+        <div class="window-modal">
+          <div class="modal-close window-modal-close">×</div>
+          <div class="window-contents window-contents-works pc-view">
+            <ul class="tab-menu">
+              <li class="tab-menu_item tab-menu_switch">
+                <span class="material-symbols-outlined">devices</span>
+              </li>
+              <li class="tab-menu_item tab is-active">
+                <div class="item_inner">
+                  <span class="material-symbols-outlined">globe</span>
+                  <span class="tab-text"><?php the_field('pc_text01'); ?> | <?php the_title(); ?></span>
+                  <div class="tab-menu_line"></div>
+                </div>
+              </li>
+              <li class="tab-menu_item tab">
+                <div class="item_inner">
+                  <span class="material-symbols-outlined">globe</span>
+                  <span class="tab-text"><?php the_field('pc_text02'); ?> | <?php the_title(); ?></span>
+                  <div class="tab-menu_line"></div>
+                </div>
+              </li>
+              <li class="tab-menu_item tab">
+                <div class="item_inner">
+                  <span class="material-symbols-outlined">globe</span>
+                  <span class="tab-text"><?php the_field('pc_text03'); ?> | <?php the_title(); ?></span>
+                  <div class="tab-menu_line"></div>
+                </div>
+              </li>
+              <li class="tab-menu_item tab">
+                <div class="item_inner">
+                  <span class="material-symbols-outlined">globe</span>
+                  <span class="tab-text"><?php the_field('pc_text04'); ?> | <?php the_title(); ?></span>
+                  <div class="tab-menu_line"></div>
+                </div>
+              </li>
+            </ul>
+            <div class="tab-content is-display">
+              <img class="pc-view"
+                src="<?php the_field('pc_img01'); ?>"
+                alt=""
+                loading="lazy" />
+              <img class="sp-view"
+                src="<?php the_field('sp_img01'); ?>"
+                alt=""
+                loading="lazy" />
+            </div>
+            <div class="tab-content">
+              <img class="pc-view"
+                src="<?php the_field('pc_img02'); ?>"
+                alt=""
+                loading="lazy" />
+              <img class="sp-view"
+                src="<?php the_field('sp_img02'); ?>"
+                alt=""
+                loading="lazy" />
+            </div>
+            <div class="tab-content">
+              <img class="pc-view"
+                src="<?php the_field('pc_img03'); ?>"
+                alt=""
+                loading="lazy" />
+              <img class="sp-view"
+                src="<?php the_field('sp_img03'); ?>"
+                alt=""
+                loading="lazy" />
+            </div>
+            <div class="tab-content">
+              <img class="pc-view"
+                src="<?php the_field('pc_img04'); ?>"
+                alt=""
+                loading="lazy" />
+              <img class="sp-view"
+                src="<?php the_field('sp_img04'); ?>"
+                alt=""
+                loading="lazy" />
+            </div>
+          </div>
         </div>
-        <div class="tab-content">
-          <img class="pc-view"
-               src="<?php echo get_theme_file_uri('/images/top/tab/pc/twenty5-pc.webp'); ?>"
-               alt=""
-               loading="lazy" />
-          <img class="sp-view"
-               src="<?php echo get_theme_file_uri('/images/top/tab/sp/twenty5-sp.webp'); ?>"
-               alt=""
-               loading="lazy" />
+        <p class="single-att">※画像をタップでページ一覧が確認できます</p>
+        <h2 class="single-title"><?php the_title(); ?></h2>
+      </div>
+      <div class="single-info works-info">
+        <div class="w-1200">
+          <div class="single-info_flex">
+            <div class="single-info_left">
+              <ul>
+                <?php if (get_field('work_date')) : ?>
+                  <li>
+                    <h3>制作期間</h3>
+                    <p>
+                      <?php the_field('work_date'); ?>
+                    </p>
+                  </li>
+                <?php endif; ?>
+                <?php if (get_field('pic')) : ?>
+                  <li>
+                    <h3>担当領域</h3>
+                    <p>
+                      <?php the_field('pic'); ?>
+                    </p>
+                  </li>
+                <?php endif; ?>
+                <?php if (get_field('use_tool')) : ?>
+                  <li>
+                    <h3>使用言語・ツール</h3>
+                    <p>
+                      <?php the_field('use_tool'); ?>
+                    </p>
+                  </li>
+                <?php endif; ?>
+                <?php if (get_field('team')) : ?>
+                  <li>
+                    <h3>制作体制</h3>
+                    <p>
+                      <?php the_field('team'); ?>
+                    </p>
+                  </li>
+                <?php endif; ?>
+              </ul>
+            </div>
+            <div class="single-info_right">
+              <ul>
+                <?php if (get_field('url')) : ?>
+                  <li>
+                    <h3>サイトURL</h3>
+                    <p>
+                      <a href="<?php the_field('url'); ?>"
+                        target="_blank"><?php the_field('url'); ?></a>
+                    </p>
+                  </li>
+                <?php endif; ?>
+                <?php if (get_field('overview')) : ?>
+                  <li>
+                    <h3>概要</h3>
+                    <p>
+                      <?php the_field('overview'); ?>
+                    </p>
+                  </li>
+                <?php endif; ?>
+                <?php if (get_field('purpose')) : ?>
+                  <li>
+                    <h3>顧客の目的・課題</h3>
+                    <p>
+                      <?php the_field('purpose'); ?>
+                    </p>
+                  </li>
+                <?php endif; ?>
+                <?php if (get_field('point')) : ?>
+                  <li>
+                    <h3>制作のポイント</h3>
+                    <p>
+                      <?php the_field('point'); ?>
+                    </p>
+                  </li>
+                <?php endif; ?>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <p class="single-att">※画像をタップでページ一覧が確認できます</p>
-    <h2 class="single-title">WEB制作/企画/プロモーションを行う会社様のホームページ</h2>
-  </div>
-  <div class="single-info works-info">
-    <div class="w-1200">
-      <div class="single-info_flex">
-        <div class="single-info_left">
-          <ul>
-            <li>
-              <h3>制作期間</h3>
-              <p>
-                <?php echo get_field('work_date'); ?>
-              </p>
-            </li>
-            <li>
-              <h3>担当領域</h3>
-              <p>
-                <?php echo get_field('pic'); ?>
-              </p>
-            </li>
-            <li>
-              <h3>使用言語・ツール</h3>
-              <p>
-                <?php echo get_field('use_tool'); ?>
-              </p>
-            </li>
-            <li>
-              <h3>制作体制</h3>
-              <p>
-                <?php echo get_field('team'); ?>
-              </p>
-            </li>
-          </ul>
-        </div>
-        <div class="single-info_right">
-          <ul>
-            <li>
-              <h3>サイトURL</h3>
-              <p>
-                <a href="https://designpianows.com/"
-                   target="_blank">https://designpianows.com/</a>
-              </p>
-            </li>
-            <li>
-              <h3>概要</h3>
-              <p>
-                ボートレース多摩川にて2021年9月27日に開催されたGIウェイキーカップ開設67周年記念の特設ページです。本案件はコーディングと更新業務のみの依頼のため、クライアントへの連絡を担当するディレクターと私コーダー2名の体制。デザインがテンプレート化されているため、あらかじめ大枠が作られた特設サイト用テンプレートファイルを編集しての制作でした。
-              </p>
-            </li>
-            <li>
-              <h3>顧客の目的・課題</h3>
-              <p>
-                ・葬儀会社様の認知度向上<br>
-                ・利用者を増やしたい<br>
-                ・既存のLP以外にもっと情報を掲載したい
-              </p>
-            </li>
-            <li>
-              <h3>制作のポイント</h3>
-              <p>
-                コーダーとして、課題である認知度向上を目的として、検索上位になるためのSEO対策やGoogleビジネスの登録を行いました。特にサイト内のSEO対策を万全にするため、SEOチェックツールを用いて検証を実施。公開後はサイト閲覧数が右肩上がりになっていき、それに伴って徐々に検索結果も上がっていきました。
-              </p>
-            </li>
-          </ul>
+      <div class="other-works">
+        <div class="w-1200">
+          <div class="other-group">
+            <h3>
+              Other Works
+              <span>その他の制作実績</span>
+            </h3>
+            <ul class="other-list">
+              <li>
+                <div class="other-img">
+                  <img src="<?php echo get_theme_file_uri('/images/top/web-app/app-sample.webp'); ?>"
+                    alt="" />
+                </div>
+                <p>ホームページタイトル | 省略名称</p>
+              </li>
+              <li>
+                <div class="other-img">
+                  <img src="<?php echo get_theme_file_uri('/images/top/web-app/app-sample.webp'); ?>"
+                    alt="" />
+                </div>
+                <p>ホームページタイトル | 省略名称</p>
+              </li>
+              <li>
+                <div class="other-img">
+                  <img src="<?php echo get_theme_file_uri('/images/top/web-app/app-sample.webp'); ?>"
+                    alt="" />
+                </div>
+                <p>ホームページタイトル | 省略名称</p>
+              </li>
+            </ul>
+            <a href="#"
+              class="btn works-btn">制作実績をもっと見る</a>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-  <div class="other-works">
-    <div class="w-1200">
-      <div class="other-group">
-        <h3>
-          Other Works
-          <span>その他の制作実績</span>
-        </h3>
-        <ul class="other-list">
-          <li>
-            <div class="other-img">
-              <img src="<?php echo get_theme_file_uri('/images/top/web-app/app-sample.webp'); ?>"
-                   alt="" />
-            </div>
-            <p>ホームページタイトル | 省略名称</p>
-          </li>
-          <li>
-            <div class="other-img">
-              <img src="<?php echo get_theme_file_uri('/images/top/web-app/app-sample.webp'); ?>"
-                   alt="" />
-            </div>
-            <p>ホームページタイトル | 省略名称</p>
-          </li>
-          <li>
-            <div class="other-img">
-              <img src="<?php echo get_theme_file_uri('/images/top/web-app/app-sample.webp'); ?>"
-                   alt="" />
-            </div>
-            <p>ホームページタイトル | 省略名称</p>
-          </li>
-        </ul>
-        <a href="#"
-           class="btn works-btn">制作実績をもっと見る</a>
-      </div>
-    </div>
-  </div>
+  <?php endwhile;
+  endif; ?>
 </main>
 <?php get_footer(); ?>
